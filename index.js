13ab93a7d069c395f9a0b11444553a861103367a
unknown <brianzioki100@gmail.com>
2025-10-26 22:41:52 +0300

    my API updates

--- index.js
+++ index.js
@@ -15,6 +15,67 @@ app.get("/users",async(req,res)=>{
     }
     
 })
+
+app.post("/users",async(req,res)=>{
+    try{
+        const {firstName,lastName,emailAddress}=req.body;
+        const user= await client.user.create({
+            data:{
+                firstName,
+                lastName,
+                emailAddress
+            }
+        })
+
+        res.status(201).json(user);
+    }catch(e){
+        res.status(500).json({message:"something went wrong"})
+    }
+})
+
+app.get("/user/:id",(req,res){
+    try{
+        const {id}=req.params;
+        const user = await client.user.findUnique({
+            where:{
+                id
+            }
+
+        })
+        if(!user){
+            res.status(404).json({message:"sorry User not found"})
+            return;
+        }
+
+    }catch{
+        res.status(500).json({message:"opp!something went wrong"})
+
+    }
+})
+
+app.delete("/user/:idOfTheUser", async function (req, res) {
+  try {
+    const { idOfTheUser } = req.params;
+
+    await client.user.update({
+      where: {
+        id: Number(idOfTheUser),
+      },
+      data: {
+        isDeleted: true,
+      },
+    });
+
+    res.status(200).send('User deleted successfully');
+  } catch (e) {
+    console.error(e);
+    res.status(500).send('Something went wrong');
+  }
+});
+
+
+
+
 const PORT=5000;
 app.listen(PORT,()=>{
     console.log(`Server is listening on port ${PORT}`)
